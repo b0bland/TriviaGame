7b9cbb14630434358297a9e6ee8c6f495e5a8b85
b0bland <alfredychan96@gmail.com>
2019-06-02 16:58:23 -0700

    Added some questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+var questions = {
+    Q1: {
+        Q: "What is the color of the sky?",
+        A: "Blue",
+        W1: "Green",
+        W2: "Purple",
+        W3: "Brown"
+    },
+    Q2: {
+        Q: "What is the radius of Earth, in miles?",
+        A: "3960 miles",
+        W1: "5250 miles",
+        W2: "2600 miles",
+        W3: "4420 miles"
+    }
+    Q3: {
+        Q: "Which football club has the most English Premier League titles?"
+        A: "Manchester United",
+        W1: "Liverpool",
+        W2: "Arsenal",
+        W3: "Manchester City"
+    }
+}
