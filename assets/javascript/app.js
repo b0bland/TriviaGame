031e26e617d1ed8f7c4bf56bf29a22e914d8242d
b0bland <alfredychan96@gmail.com>
2019-06-03 01:03:01 -0700

    Added countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,33 @@
 //Set questions and possble answers in variable
 $(document).ready(function() {
 
-var questions = ["What is the color of the sky?", "What is the radius of Earth, in miles?", "Which football club has the most English Premier League titles?"]
+var questions = ["What is the color of the sky?", "What is the radius of Earth, in miles?", "Which football club has the most English Premier League titles?"];
+
+
 
 
 //Main body is just start button. Next step happens when button is pressed, time also starts. 
 
 //On click, push the questions in a div in ".questions", replacing the start button
 $("#start").on("click", function() {
-    $("#questions").html(" ")
+    var timer = 30;
+    var int = setInterval(ctdn, 1000);
+    $("#time").html("Seconds remaining: " + timer);
+    $("#questions").html(" ");
 for (i=0;i<questions.length;i++) {
-    $("#questions").append("<div class='Q" + (i+1) + "'>" + questions[i] + "</div>")
-}
+    $("#questions").append("<div class='str' id='Q" + (i+1) + "'>" + questions[i] + "</div>")
+};
+
+
+function ctdn() {
+    timer = timer - 1;
+    if (timer <= 0) {
+        clearInterval(int);
+        //results
+        return;
+    }
+    $("#time").html("Seconds remaining: " + timer)
+};
 
 })
 
