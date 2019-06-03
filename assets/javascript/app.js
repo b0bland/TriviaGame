31b6728a5453bb1415988283e344c6bef7fd2f5e
b0bland <alfredychan96@gmail.com>
2019-06-02 19:05:42 -0700

    Added pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+//Set questions and possble answers in variable
+
 var questions = {
     Q1: {
         Q: "What is the color of the sky?",
@@ -12,12 +14,27 @@ var questions = {
         W1: "5250 miles",
         W2: "2600 miles",
         W3: "4420 miles"
-    }
+    },
     Q3: {
-        Q: "Which football club has the most English Premier League titles?"
+        Q: "Which football club has the most English Premier League titles?",
         A: "Manchester United",
         W1: "Liverpool",
         W2: "Arsenal",
         W3: "Manchester City"
     }
 }
+
+//Main body is just start button. Next step happens when button is pressed, time also starts. 
+
+//On click, push the questions in a div in ".questions", replacing the start button
+
+//The possible answers should go under the corresponding questions div 
+
+//Make the questions into multiple choice boxes that can be checked, but only one per question. So the buttons must be grouped per question div. 
+
+//At time up, or when submit is pressed, resulted are checked to see if right or wrong. 
+
+//Store correct or incorrect into a variable
+
+//Present results info, give button to restart 
+
